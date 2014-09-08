a8e47024b41279c27baa629c90c178b6743399e4
azelinskyy <azelinskyy@malkosua.com>
2014-09-08 23:19:33 +0300

    Added override of ToString() method in City class.
    This fixes display of the city name in PDF

--- TP1/Models/DomainModels/City.cs
+++ TP1/Models/DomainModels/City.cs
@@ -6,25 +6,39 @@
 //   The city.
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
-
 namespace Model.DomainModels
 {
     /// <summary>
-    /// The city.
+    ///     The city.
     /// </summary>
     public class City
     {
         #region Public Properties
 
         /// <summary>
-        /// Gets or sets the id.
+        ///     Gets or sets the id.
         /// </summary>
         public int Id { get; set; }
 
         /// <summary>
-        /// Gets or sets the name.
+        ///     Gets or sets the name.
         /// </summary>
         public string Name { get; set; }
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// String reprosentations of the city name
+        /// </summary>
+        /// <returns>
+        /// Name of the city <see cref="string"/>.
+        /// </returns>
+        public override string ToString()
+        {
+            return this.Name;
+        }
 
         #endregion
     }
