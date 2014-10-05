4ef386edd7ac4c5aa03578189f765b3513a28412
azelinskyy <azelinskyy@malkosua.com>
2014-10-05 18:04:59 +0300

    Changed text of project export tab

--- TP1/TP1/Helpers/KnockoutHtmlHelper.cs
+++ TP1/TP1/Helpers/KnockoutHtmlHelper.cs
@@ -1,6 +1,7 @@
 ﻿namespace System.Web.Mvc.Html
 {
     using System;
+    using System.Linq;
     using System.Linq.Expressions;
     using System.Web.Mvc;
 
@@ -18,6 +19,11 @@
             return Tag(name, tagName, additionBinders);
         }
 
+        public static MvcHtmlString KnockoutHelperFor(this HtmlHelper helper, string name, string tagName = "label", string additionBinders = "", params string[] values)
+        {
+            return Tag(name, tagName, additionBinders, values);
+        }
+
         public static MvcHtmlString Tag(string name, string tagName, string additionBinders = "")
         {
             var tag = new TagBuilder(tagName);
@@ -27,6 +33,18 @@
             }
 
             tag.Attributes.Add("data-bind", String.Format("text: language().{0}{1}", name, additionBinders));
+            return new MvcHtmlString(tag.ToString());
+        }
+
+        public static MvcHtmlString Tag(string name, string tagName, string additionBinders = "", params string[] values)
+        {
+            var tag = new TagBuilder(tagName);
+            if (additionBinders != string.Empty)
+            {
+                additionBinders = ", " + additionBinders;
+            }
+
+            tag.Attributes.Add("data-bind", String.Format("text: String.format(language().{0}{1},{2})", name, additionBinders, string.Join(", ", values)));
             return new MvcHtmlString(tag.ToString());
         }
 
