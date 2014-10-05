00274b76748d584f7eb2f557a6f6f3ab445a5667
azelinskyy <azelinskyy@malkosua.com>
2014-10-05 16:37:05 +0300

    Added support for String.format in js

--- TP1/TP1/Scripts/Internationalization/base.js
+++ TP1/TP1/Scripts/Internationalization/base.js
@@ -42,3 +42,15 @@ var languageModel = function (languages) {
 
     });
 };
+
+if (!String.format) {
+    String.format = function () {
+        var s = arguments[0];
+        for (var i = 0; i < arguments.length - 1; i++) {
+            var reg = new RegExp("\\{" + i + "\\}", "gm");
+            s = s.replace(reg, arguments[i + 1]);
+        }
+
+        return s;
+    }
+}
