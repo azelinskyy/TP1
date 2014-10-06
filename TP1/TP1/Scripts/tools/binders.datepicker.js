81538ef0e2fc2c349d02a98249c4d7eab3e81c02
gregory-hasyn <gregory.hasyn@gmail.com>
2014-10-06 12:12:51 +0300

    Add datepicker icon support.

--- TP1/TP1/Scripts/tools/binders.datepicker.js
+++ TP1/TP1/Scripts/tools/binders.datepicker.js
@@ -1,6 +1,13 @@
 ﻿ko.bindingHandlers.datepicker = {
     init: function (element, valueAccessor, allBindingsAccessor) {
         var options = allBindingsAccessor().datepicker || {};
-        $(element).datepicker(options);
+        var dateInput = $(element);
+        dateInput.datepicker(options);
+
+        dateInput.next('#btn').click(function () {
+            $(document).ready(function () {
+                dateInput.datepicker().focus();
+            });
+        });
     }
 };
